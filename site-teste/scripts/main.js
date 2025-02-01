cfeaeee89b6aead9dbd55b31a9f373bd8ddc0cf7
iasmynlp18i <iasminlopes71@gmail.com>
2025-02-01 19:20:21 -0300

    alterando as cores e imagens

--- site-teste/scripts/main.js
+++ site-teste/scripts/main.js
@@ -4,14 +4,14 @@ const myImage = document.querySelector("img");
 myImage.onclick = () => {
 
    const mySrc = myImage.getAttribute("src");
-   if (mySrc === "imagens/OIP.jpg") {
-      myImage.setAttribute("src", "imagens/halnaker-tunnel-inglaterra-paul-benns.jpg");
-      myImage.setAttribute("alt", "Uma floresta com tons verdes chamativos reluzindo a luz do sol entre suas árvores");
+   if (mySrc === "imagens/OIP(2).jpg") {
+      myImage.setAttribute("src", "https://fossiltrace.com/wp-content/uploads/2020/02/4EarlyMorningFall2016-scaled.jpeg");
+      myImage.setAttribute("alt", "A esquerda um feixe de sol reluzindo entre o alto tronco de uma árvore de folhas verdes. A direita um caminho com tons verdes chamativos e árvores de outono alaranjadas.");
    }
 
    else {
-      myImage.setAttribute("src", "imagens/OIP.jpg");
-      myImage.setAttribute("alt", "Uma passagem de formato redondo envolta por árvores amarelas curvadas");
+      myImage.setAttribute("src", "imagens/OIP(2).jpg");
+      myImage.setAttribute("alt", "Uma casa de madeira no fundo da imagem envolta por árvores altas que reluzem feixes de sol em seus troncos");
    }
 
 // Mensagem de boas vindas personalizada
